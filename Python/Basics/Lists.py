210dc844c3e4c35f763178071205fa0883aef2b6
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2020-05-20 23:41:56 +0530

    Create Lists.py

---
+++ Python/Basics/Lists.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    N = int(input())
+    l = list()
+    for i in range(N):
+        a = input().split()
+        if a[0] == 'insert':
+            l.insert(int(a[1]), int(a[2]))
+        elif a[0] == 'append':
+            l.append(int(a[1]))
+        elif a[0] == 'print':
+            print(l)
+        elif a[0] == 'sort':
+            l.sort()
+        elif a[0] == 'remove':
+            l.remove(int(a[1]))
+        elif a[0] == 'pop':
+            l.pop()
+        elif a[0] == 'reverse':
+            l.reverse()
