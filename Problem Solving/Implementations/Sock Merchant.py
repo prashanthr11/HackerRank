c0d481f904177c51176ff0553acef2e511a28447
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2020-06-07 21:54:28 +0530

    Create Sock Merchant.py

---
+++ Problem Solving/Implementations/Sock Merchant.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    d = Counter(ar)
+    for k, v in d.items():
+        d[k] %= 2
+    t = sum(list(d.values()))
+    return int((n - t) / 2)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
