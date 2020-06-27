b9bc07068749ddb7335ba6bb462c338765e3fae0
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2020-06-27 23:27:37 +0530

    Create Save the Prisoner!.py

---
+++ Problem Solving/Implementations/Save the Prisoner!.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def saveThePrisoner(n, m, s):
+    x = (m + s - 1) % n
+    if x:
+        return x
+    else:
+        return n
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nms = input().split()
+
+        n = int(nms[0])
+
+        m = int(nms[1])
+
+        s = int(nms[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
