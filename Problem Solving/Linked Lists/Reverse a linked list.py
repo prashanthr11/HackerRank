d0f474420bcf15f7629b0f26e980be9e4d0c9fa3
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2020-08-25 20:06:41 +0530

    Added HR LinkedList Solutions

---
+++ Problem Solving/Linked Lists/Reverse a linked list.py
@@ -0,0 +1,82 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the reverse function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def reverse(head):
+    if head:
+        temp = head
+        l = list()
+        while temp:
+            l.append(temp)
+            temp = temp.next
+        l[0].next =None
+        head = l[-1]
+        l = l[::-1]
+        for i in range(len(l) - 1):
+            l[i].next = l[i+1]
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
